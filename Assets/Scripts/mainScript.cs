5f3fb0bc08e49a9331e807d0acc46ff369ef5f8f
Batikanhyt <batikanhyt@gmail.com>
2018-04-17 12:30:04 +0300

    Profile

---
+++ Assets/Scripts/mainScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+using System.Text.RegularExpressions;
+public class mainScript : MonoBehaviour {
+
+	private string sceeneName = "Profile";
+	public void profileButton(){
+		Application.LoadLevel (sceeneName);
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+}
--- Assets/Scripts/statistic.cs
+++ Assets/Scripts/statistic.cs
@@ -6,22 +6,40 @@ using System.Text.RegularExpressions;
 
 public class statistic : MonoBehaviour {
     public Text Username;
+	public Text Mail;
     public Text Level1;
     public Text Level2;
-    public Text currentLevel;
-	public string username = Login.user;
+    public Text status;
+	//public string uname = Login.user;
+	private string [] splitter;
+	private int result;
 
     //string statURL = "https://lechief.azurewebsites.net/statistic.php";
 	string statURL = "localhost/lechief/statistic.php";
     public string changed;
     // Use this for initialization
+	public void gobackButton(){
+		Application.LoadLevel("Main");
+	}
     void Start () {
-		Username.text = username;
+		Username.text = Login.user;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-            StartCoroutine(stat(username));
+            StartCoroutine(stat(Login.user));
+
+		result = int.Parse(splitter [1]) + int.Parse(splitter [3]);
+		if (result > 0 && result < 25) {
+			status.text = "Newbie";
+		} else if (result > 25 && result < 100)
+			status.text = "Beginner";
+		else if (result > 100 && result < 200)
+			status.text = "ıntermediate";
+		else if (result > 200 && result < 300)
+			status.text = "Veteran";
+		else 
+			status.text = "";
     }
 
     IEnumerator stat(string uname)
@@ -41,5 +59,9 @@ public class statistic : MonoBehaviour {
 		WWW site = new WWW(statURL,form);
 		yield return site;
 		Debug.Log (site.text);
+		splitter = site.text.Split(char.Parse(","));
+		Level1.text = "Level"+splitter [0] + " Your accuracy is : " + splitter [1];
+		Level2.text = "Level"+splitter [2] + " Your Accuracy is : " + splitter [3];
+		Mail.text = splitter [4];
     }
 }
