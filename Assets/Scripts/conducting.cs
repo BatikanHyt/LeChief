faa9b0e13543aeb47c1453a7d3557615b91d42ce
Batikanhyt <batikanhyt@gmail.com>
2018-05-02 14:47:14 +0300

    s

---
+++ Assets/Scripts/conducting.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+using System.Text.RegularExpressions;
+using UnityEngine.SceneManagement;
+public class conducting : MonoBehaviour {
+
+	public void redirectMain(){
+		SceneManager.LoadScene ("Main");
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
