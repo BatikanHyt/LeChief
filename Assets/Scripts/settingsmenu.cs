7b10cbe8175443d66763d5b983e1b2d02c6a90f8
Batikanhyt <batikanhyt@gmail.com>
2018-05-09 02:37:30 +0300

    Settings

--- Assets/Scripts/settingsmenu.cs
+++ Assets/Scripts/settingsmenu.cs
@@ -7,8 +7,15 @@ using UnityEngine.SceneManagement;
 
 public class settingsmenu : MonoBehaviour {
 	public AudioMixer audioMixer;
-
+	public InputField pass;
+	public InputField passnew;
+	public InputField passre;
+	public Text invalid;
+	private string Spass;
+	private string Spassnew;
+	private string Spassre;
 	public Dropdown resolutionDropdown;
+	private string changePassUrl = "localhost/lechief/updatePassword.php";
 	Resolution[] resolutions;
 	void Start(){
 		resolutions = Screen.resolutions;
@@ -28,6 +35,40 @@ public class settingsmenu : MonoBehaviour {
 		resolutionDropdown.value = currentResolutionındex;
 		resolutionDropdown.RefreshShownValue ();
 	}
+	public void saveChanges(){
+		if (Spass != "" && Spassnew != "" && Spassre != "" && Spassnew == Spassre) {
+			invalid.text = " ";
+			StartCoroutine (changePassword (Login.user, Spass, Spassnew));
+		} else if (Spassnew != Spassre) {
+			invalid.text = "Passwords dont match";
+		}else if (Spass == "")
+			invalid.text = "Please enter your password's credentials";
+	}
+	void Update(){
+		if (Input.GetKeyDown (KeyCode.Tab)) {
+			if (pass.isFocused) {
+				passnew.Select ();
+			}
+			if (passnew.isFocused) {
+				passre.Select ();
+			}
+			if (passre.isFocused) {
+				pass.Select ();
+			}
+		}
+		if (Input.GetKeyDown (KeyCode.Return)) {
+			if (Spass != "" && Spassnew != "" && Spassre != "" && Spassnew == Spassre) {
+				invalid.text = " ";
+				saveChanges ();
+			} else if (Spassnew != Spassre) {
+				invalid.text = "Passwords dont match";
+			}else 
+				invalid.text = "Please enter your password's credentials";
+		}
+		Spass = pass.text;
+		Spassnew = passnew.text;
+		Spassre = passre.text;
+	}
 	public void returnMenu(){
 		SceneManager.LoadScene ("Main");
 		
@@ -38,10 +79,20 @@ public class settingsmenu : MonoBehaviour {
 	}
 
 	public void SetVolume (float volume){
-		Debug.Log (volume);
 		audioMixer.SetFloat ("volume", volume);
 	}
 	public void SetFullScreen(bool isFullScreen){
 		Screen.fullScreen = isFullScreen;
+	}
+	IEnumerator changePassword(string username, string pass, string newpass){
+		WWWForm form = new WWWForm();
+		form.AddField("usernamePost", username);
+		form.AddField("oldPost", pass);
+		form.AddField("passwordPost", newpass);
+
+		WWW site = new WWW(changePassUrl,form);
+		yield return site;
+		//Debug.Log ("Serverdan gelen : " + site.text);
+		invalid.text = site.text;
 	}
 }
